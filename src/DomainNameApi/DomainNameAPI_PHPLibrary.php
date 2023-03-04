37622f4ae4ec163360ffe97cd994d2ce9b5dbdaf
Bünyamin AKÇAY <bakcay@live.com>
2023-03-04 17:54:16 +0300

    composer test

--- src/DomainNameApi/DomainNameAPI_PHPLibrary.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: bunyaminakcay
- * Project name php-dna
- * 4.03.2023 17:06
- * Bünyamin AKÇAY <bunyamin@bunyam.in>
- */
-require_once __DIR__.'/../../dna.php';
